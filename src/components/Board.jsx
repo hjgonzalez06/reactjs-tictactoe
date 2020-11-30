60339fc599fc4d06a4e615bede97291ef51f68d2
Hiram J. González <hiramjgonzalez98@gmail.com>
2020-11-30 02:01:20 -0400

    Lift state up to Game component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,64 +1,18 @@
 import React, { useState } from 'react';
 import Square from './Square';
 
-const Board = () => {
-
-    let [squares, setSquares] = useState(Array(9).fill(null));
-    let [xIsNext, setXIsNext] = useState(true);
+const Board = ({ squares, onClick}) => {
 
     const renderSquare = i => {
         return <Square
                     value={squares[i]}
-                    onClick={() => handleClick(i)}
+                    onClick={() => onClick(i)}
                 />
-    }
-
-    const handleClick = i => {
-        const newSquares = squares.slice();
-        if(calculateWinner(squares) || squares[i]) return;
-        newSquares[i] = getNextPlayer();
-        setSquares(newSquares);
-        setXIsNext(!xIsNext);
-    }
-
-    const getCurrentPlayer = () => {
-        return xIsNext ? 'O' : 'X';
-    }
-
-    const getNextPlayer = () => {
-        return xIsNext ? 'X' : 'O';
-    }
-    
-    const getGameStatus = () => {
-        let winner = calculateWinner(squares);
-
-        return winner ? `Winner: ${getCurrentPlayer()}` : `Next player: ${getNextPlayer()}`;
-    }
-    
-    const calculateWinner = () => {
-        const lines = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
-    
-        return lines.find(line => {
-            const [a, b, c] = line;
-            return (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]);
-        });
     };
-
-    const gameStatus = getGameStatus();
 
     return (
         <>
             <div>
-                <div className="status">{gameStatus}</div>
                 <div className="board-row">
                     {renderSquare(0)}
                     {renderSquare(1)}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './Board';
 
 const Game = () => {
+
+    let [history, setHistory] = useState([
+        {
+            squares: Array(9).fill(null)
+        }
+    ]);
+    let [stepNumber, setStepNumber] = useState(0);
+    let [xIsNext, setXIsNext] = useState(true);
+    const newHistory = history;
+    const currentSquares = newHistory[newHistory.length-1].squares;
+    const gameStatus = getGameStatus(currentSquares, xIsNext);
+
+    const handleClick = i => {
+        const newHistory = history;
+        const currentSquares = newHistory[newHistory.length-1].squares;
+        const newSquares = currentSquares.slice();
+        if(calculateWinner(newSquares) || newSquares[i]) return;
+        newSquares[i] = getNextPlayer(xIsNext);
+        setHistory(newHistory.concat([
+            {
+                squares: newSquares
+            }
+        ]));
+        setXIsNext(!xIsNext);
+    }
 
     return (
         <>
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board 
+                        squares={currentSquares}
+                        onClick={i => handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
                     <div>
-                        Status
+                        {gameStatus}
                     </div>
                     <div>
                         Moves
@@ -21,6 +49,38 @@ const Game = () => {
         </>
     );
 
+};
+
+const getCurrentPlayer = xIsNext => {
+    return xIsNext ? 'O' : 'X';
+}
+
+const getNextPlayer = xIsNext => {
+    return xIsNext ? 'X' : 'O';
+}
+
+const getGameStatus = (squares, xIsNext) => {
+    let winner = calculateWinner(squares);
+
+    return winner ? `Winner: ${getCurrentPlayer(xIsNext)}` : `Next player: ${getNextPlayer(xIsNext)}`;
+}
+
+const calculateWinner = squares => {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    return lines.find(line => {
+        const [a, b, c] = line;
+        return (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]);
+    });
 };
 
 export default Game;
